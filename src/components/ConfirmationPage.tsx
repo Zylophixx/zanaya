735da15a8bbc57dbe06d94cf916d084c77252a27
Zylophixx <zylophixx@gmail.com>
2025-08-25 19:25:21 +0530

    Updated ConfirmationPage.tsx

--- src/components/ConfirmationPage.tsx
+++ src/components/ConfirmationPage.tsx
@@ -54,12 +54,6 @@ export function ConfirmationPage() {
         >
           Make Another Booking
         </button>
-
-  {/* Emergency Contact */}
-        <div className="bg-red-50 rounded-lg p-4 border border-red-200">
-          <p className="text-red-800 font-medium">Emergency Contact</p>
-          <p className="text-red-700 text-sm">For urgent matters, call directly: +91 8273441052</p>
-        </div>
         
       </div>
     </div>
